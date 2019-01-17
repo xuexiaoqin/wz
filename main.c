6fb8b7783a2fe3bbcab0aec3e2bb013c78e56ec4
jj <xuexiaoqin@qq.com>
2019-01-17 13:20:13 +0800

    够建一个基本框架

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+struct Point
+{
+    int x;
+    int y;
+};
+int index = 0;
+struct Point arr[100];
 
 int main(){
 
-    printf("hello world!");
+   printf("欢迎进入专场位置管理系统!\n");
+    while (1)
+    {
+        printf("1---添加车辆位置\n");
+        printf("2---删除车辆位置\n");
+        printf("3---显示全部车辆位置\n");
+        printf("4---返回离我的位置最近的车辆\n");
+        printf("5---退出\n");
+        printf("请选择相应功能\n");
+        int code;
+        scanf("%d", &code);
+        if (code == 1)
+        {
+            
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            break;
+        }
+    }
+
 
     return 0;
 
